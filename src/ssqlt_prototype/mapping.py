05f25b0998978f03b9033dee756bab5a793763b2
Aidan Bailey <dev@aidanbailey.me>
2024-09-17 22:13:29 +0200

    Add  sql method

--- src/ssqlt_prototype/mapping.py
+++ src/ssqlt_prototype/mapping.py
@@ -1,6 +1,7 @@
 import os
 from dataclasses import dataclass
 from typing import Self
+from string import Template
 
 
 @dataclass
@@ -8,7 +9,7 @@ class Mapping:
     schema: str
     source_tables: list[str]
     target_table: str
-    sql_template: str
+    sql_template: Template
 
     @classmethod
     def from_file(cls, file_path: str) -> Self:
@@ -18,10 +19,19 @@ class Mapping:
         target_table = tokens[-2]
         source_tables = tokens[1:-2]
         with open(file_path, "r") as f:
-            sql_template = f.read().strip()
+            sql_template = Template(f.read().strip())
         return cls(
             schema=schema,
             source_tables=source_tables,
             target_table=target_table,
             sql_template=sql_template,
         )
+
+    def sql(self, custom_source_tables: list[str] | None = None) -> str:
+        source_tables = custom_source_tables or self.source_tables
+        if len(source_tables) != self.source_tables:
+            raise ValueError(f"Number of source tables is insufficient: expected {len(self.source_tables)}, got {len(source_tables)}")
+        subsitution_mapping = {}
+        for i, table in enumerate(source_tables):
+            subsitution_mapping[f"S{i}"] = table
+        return self.sql_template.substitute(subsitution_mapping)
