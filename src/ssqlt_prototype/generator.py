bb1cacfd8af7b8b3eca333dfb947b3adcc1b00dc
Aidan Bailey <dev@aidanbailey.me>
2024-09-17 23:15:28 +0200

    Add initial steps to generating the transducer

--- src/ssqlt_prototype/generator.py
+++ src/ssqlt_prototype/generator.py
@@ -13,3 +13,45 @@ class Generator:
     def from_dir(cls, path: str) -> Self:
         context = Context.from_dir(path)
         return cls(context)
+
+    def generate(self) -> str:
+        transducer = ""
+
+        ##########
+        # SOURCE #
+        ##########
+
+        # STEP 1: Write source table create
+
+        transducer += "/* SOURCE TABLE */\n\n"
+
+        transducer += self.context.source_table.sql + "\n\n"
+
+        # STEP 2: Write source constraints
+
+        transducer += "/* SOURCE CONSTRAINTS */\n\n"
+
+        for constraint in self.context.source_constraints:
+            transducer += constraint.generate_function() + "\n\n"
+            transducer += constraint.generate_trigger() + "\n\n\n"
+
+        # STEP 3: Write target table creates
+
+        transducer += "/* TARGET TABLES */\n\n"
+
+        for table in self.context.target_tables:
+            transducer += table.sql + "\n\n"
+
+        # STEP 4: Write target table constraints
+
+        transducer += "/* TARGET CONSTRAINTS */\n\n"
+
+        for constraint in self.context.target_constraints:
+            transducer += constraint.generate_function() + "\n\n"
+            transducer += constraint.generate_trigger() + "\n\n\n"
+
+        return transducer
+
+    def generate_to_path(self, path: str):
+        with open(path, "w") as f:
+            f.write(self.generate())
