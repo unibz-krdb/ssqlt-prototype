f9165d416b35b5d82734ad8ee64a09bce7979fa4
Aidan Bailey <dev@aidanbailey.me>
2024-09-17 23:14:57 +0200

    Add output to test

--- test/test_generator.py
+++ test/test_generator.py
@@ -3,4 +3,5 @@ from fixtures import resource_dir as resource_dir, input_dir as input_dir
 from ssqlt_prototype.generator import Generator
 
 def test_from_dir(input_dir):
-    _ = Generator.from_dir(input_dir)
+    generator = Generator.from_dir(input_dir)
+    generator.generate_to_path("output.sql")
