a875d02de6169b38e501a3e9e714259ad77d144b
Aidan Bailey <dev@aidanbailey.me>
2024-09-17 21:30:30 +0200

    Add test_constraint.py

---
+++ test/test_constraint.py
@@ -0,0 +1,15 @@
+import os
+from fixtures import resource_dir as resource_dir, input_dir as input_dir
+
+from ssqlt_prototype.constraint import Constraint
+
+def test_from_file(input_dir):
+    file_path = os.path.join(input_dir, "constraints", "source", "transducer._person.cfd.1.insert.sql")
+    constraint = Constraint.from_file(file_path)
+    assert constraint.schema == "transducer"
+    assert constraint.table == "_person"
+    assert constraint.type_ == "cfd"
+    assert constraint.index == 1
+    assert constraint.stage == "insert"
+    with open(file_path, "r") as f:
+        assert constraint.sql == f.read().strip()
