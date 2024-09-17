31554a23f123bfe0242fd30c75b3a548cc2ce729
Aidan Bailey <dev@aidanbailey.me>
2024-09-17 22:05:14 +0200

    Add test_mapping.py

---
+++ test/test_mapping.py
@@ -0,0 +1,13 @@
+import os
+from fixtures import resource_dir as resource_dir, input_dir as input_dir
+
+from ssqlt_prototype.mapping import Mapping
+
+def test_from_file(input_dir):
+    file_path = os.path.join(input_dir, "mappings", "target", "transducer._person._city.sql")
+    mapping = Mapping.from_file(file_path)
+    assert mapping.schema == "transducer"
+    assert mapping.source_tables == ["_person"]
+    assert mapping.target_table == "_city"
+    with open(file_path, "r") as f:
+        assert mapping.sql_template == f.read().strip()
