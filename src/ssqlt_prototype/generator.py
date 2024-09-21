cf7398bf38a96a856c6fc07d56c83dee7d934bd4
Aidan Bailey <dev@aidanbailey.me>
2024-09-21 22:44:39 +0200

    Add insert table functions and triggers

--- src/ssqlt_prototype/generator.py
+++ src/ssqlt_prototype/generator.py
@@ -12,12 +12,13 @@ class Generator:
 
     def __init__(self, context: Context) -> None:
         self.context = context
-        self.insert_tables = {context.source_table.table: InsertTable(context.source_table)}
-        self.delete_tables = {context.source_table.table: DeleteTable(context.source_table)}
+        self.insert_tables = {context.source_table.table: InsertTable(source=context.source_table, mapping=context.source_mapping)}
+        self.delete_tables = {context.source_table.table: DeleteTable(source=context.source_table)}
         self.schema = context.source_table.schema
         for table in context.target_tables:
-            self.insert_tables[table.table] = InsertTable(table)
-            self.delete_tables[table.table] = DeleteTable(table)
+            mapping = next(mapping for mapping in context.target_mappings if mapping.target_table.lower() == table.table.lower())
+            self.insert_tables[table.table] = InsertTable(source=table, mapping=mapping)
+            self.delete_tables[table.table] = DeleteTable(source=table)
 
     @classmethod
     def from_dir(cls, path: str) -> Self:
@@ -66,7 +67,7 @@ class Generator:
 
         for table in self.insert_tables:
             insert_table = self.insert_tables[table]
-            transducer += insert_table.createSql() + "\n\n"
+            transducer += insert_table.create_sql() + "\n\n"
 
         # STEP 6: Write delete table
 
@@ -80,8 +81,16 @@ class Generator:
 
         transducer += "/* LOOP PREVENTION MECHANISM */\n\n"
 
-        transducer += f"CREATE TABLE {self.schema}._LOOP (loop_start INT NOT NULL );"
+        transducer += f"CREATE TABLE {self.schema}._LOOP (loop_start INT NOT NULL );\n\n"
 
+        # STEP 8: Write insert functions
+
+        transducer += f"/* INSERT FUNCTIONS & TRIGGERS */\n\n"
+
+        for table in self.insert_tables:
+            insert_table = self.insert_tables[table]
+            transducer += insert_table.generate_function() + "\n\n"
+            transducer += insert_table.generate_trigger() + "\n\n"
 
         return transducer
 
--- src/ssqlt_prototype/insert_table.py
+++ src/ssqlt_prototype/insert_table.py
@@ -1,20 +1,49 @@
 from dataclasses import dataclass
 
 from .create_table import CreateTable
+from .mapping import Mapping
 
 
 @dataclass
 class InsertTable:
     source: CreateTable
+    mapping: Mapping
     schema: str
     table: str
 
-    def __init__(self, source: CreateTable) -> None:
+    def __init__(self, source: CreateTable, mapping: Mapping) -> None:
         self.source = source
         self.table = source.table + "_INSERT"
+        self.mapping = mapping
 
-    def createSql(self) -> str:
+    def create_sql(self) -> str:
         sql = f"CREATE TABLE {self.source.schema}.{self.table} AS\n"
         sql += f"SELECT * FROM {self.source.schema}.{self.source.table}\n"
         sql += "WHERE 1<>1;"
+        return sql
+
+    def generate_function(self) -> str:
+        sql = f"""CREATE OR REPLACE FUNCTION {self.source.schema}.{self.table}_insert_fn()
+   RETURNS TRIGGER LANGUAGE PLPGSQL AS $$
+   BEGIN
+   IF EXISTS (SELECT * FROM {self.source.schema}._loop) THEN
+      DELETE FROM {self.source.schema}._loop;
+      DELETE FROM {self.source.schema}._person_insert;
+      RETURN NULL;
+   ELSE
+      INSERT INTO {self.source.schema}._loop VALUES (-1);
+      INSERT INTO {self.source.schema}.{self.table} VALUES(SELECT * from NEW);
+      RETURN NEW;
+   END IF;
+END;  $$;
+"""
+
+        return sql
+
+    def generate_trigger(self) -> str:
+        sql = f"""CREATE TRIGGER {self.source.schema}.{self.table}_trigger
+AFTER INSERT ON {self.source.schema}.{self.source.table}
+FOR EACH ROW
+EXECUTE FUNCTION {self.source.schema}.{self.table}_insert_fn();
+        """
         return sql
