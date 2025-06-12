21279cdb5bc6d6e5c55d9602e5f7a74705dea750
Aidan Bailey <dev@aidanbailey.me>
2025-06-12 10:34:15 +0200

    Fix tests

--- test/test_create_table.py
+++ test/test_create_table.py
@@ -7,7 +7,7 @@ from ssqlt_prototype.TransducerContext.Dataclasses.enums import SourceTarget
 
 def test_from_file(input_dir):
     file_path = os.path.join(input_dir, "source", "create", "transducer._empdep.sql")
-    create_table = CreateTable.from_file(file_path, source_target=SourceTarget.SOURCE)
+    create_table = CreateTable.from_file(file_path)
     assert create_table.schema == "transducer"
     assert create_table.table == "_empdep"
     with open(file_path, "r") as f:
